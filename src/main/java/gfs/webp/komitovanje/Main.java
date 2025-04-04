aabfe80c2dfe3ea8e65ddde641caa7b12f04bc4e
Novke <novke.brate@gmail.com>
2025-04-04 12:16:00 +0200

    main metoda

---
+++ src/main/java/gfs/webp/komitovanje/Main.java
@@ -0,0 +1,11 @@
+package gfs.webp.komitovanje;
+
+import gfs.webp.komitovanje.kalk.Kalkulator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Double resnje = Kalkulator.oduzmi(5.0,3.0);
+        System.out.println(resnje);
+    }
+}
