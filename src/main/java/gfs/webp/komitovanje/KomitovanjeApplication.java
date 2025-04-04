58821dbfd01332296edbcbab5962df0afcbfd247
Novke <novke.brate@gmail.com>
2025-04-04 11:52:33 +0200

    Inicijalizacija

---
+++ src/main/java/gfs/webp/komitovanje/KomitovanjeApplication.java
@@ -0,0 +1,13 @@
+package gfs.webp.komitovanje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomitovanjeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KomitovanjeApplication.class, args);
+    }
+
+}
